847268d5e283e013233e9e35eabc12d272dc2077
Donita <mathisdonita@gmail.com>
2018-10-08 01:52:55 -0400

    feat: fixed error in functionality

--- src/Components/Book.js
+++ src/Components/Book.js
@@ -16,11 +16,7 @@ class Book extends Component {
         <div className="book-top">
           <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${this.props.book.imageLinks.thumbnail})` }}></div>
           <div className="book-shelf-changer">
-          <select 
-              onChange={(e) => {
-                this.props.onChangeShelf(this.props.book, e.target.value) 
-              }}
-              value={this.props.book.shelf}>
+              <select onChange={this.onChangeShelf} defaultValue={this.state.books}>
             <option value="move" disabled>Move to...</option>
             <option value="currentlyReading">Currently Reading</option>
             <option value="wantToRead">Want to Read</option>
