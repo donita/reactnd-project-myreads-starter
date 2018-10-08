b29bf52669a5f6927f9b783a9c18a002748e1e03
Donita <mathisdonita@gmail.com>
2018-10-08 01:46:50 -0400

    feat: fix self change error

--- src/Components/Book.js
+++ src/Components/Book.js
@@ -1,33 +1,38 @@
 import React, {Component} from 'react'
+import PropTypes from 'prop-types'
 
 class Book extends Component {
+  static propTypes = {
+    book: PropTypes.object.isRequired,
+    onChangeShelf: PropTypes.func.isRequired
+  }
+
+
 
   render() {
-    const { books, shelf, toggleBookShelf } = this.props;
-
     return (
-        <ol className="books-grid">
-        {books.filter( book => book.shelf === shelf).map( book => (
-          <li key={book.id}>
-            <div className="book">
-              <div className="book-top">
-                <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks.thumbnail})` }}></div>
-                <div className="book-shelf-changer">
-                  <select value={book.shelf} onChange={(event) => toggleBookShelf(event, book) }>
-                    <option value="none" disabled>Move to...</option>
-                    <option value="currentlyReading">Currently Reading</option>
-                    <option value="wantToRead">Want to Read</option>
-                    <option value="read">Read</option>
-                    <option value="none">None</option>
-                  </select>
-                </div>
-              </div>
-              <div className="book-title">{book.title}</div>
-              <div className="book-authors">{book.authors}</div>
-            </div>
-          </li>
-        ))}
-      </ol>
+      <li>
+        <div className="book">
+        <div className="book-top">
+          <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${this.props.book.imageLinks.thumbnail})` }}></div>
+          <div className="book-shelf-changer">
+          <select 
+              onChange={(e) => {
+                this.props.onChangeShelf(this.props.book, e.target.value) 
+              }}
+              value={this.props.book.shelf}>
+            <option value="move" disabled>Move to...</option>
+            <option value="currentlyReading">Currently Reading</option>
+            <option value="wantToRead">Want to Read</option>
+            <option value="read">Read</option>
+            <option value="none">None</option>
+          </select>
+          </div>
+        </div>
+        <div className="book-title">{this.props.book.title}</div>
+        <div className="book-authors">{this.props.book.authors && this.props.book.authors.map(author => author).filter(authors => authors.length > 1)}</div>
+        </div>
+      </li>
     )
   }
 }
