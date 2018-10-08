28a6eb4458ba8536e6403667ec996049403dda0c
Donita <mathisdonita@gmail.com>
2018-10-08 04:58:33 -0400

    fix: fixed buggs in book shelf

--- src/Components/ListBooks.js
+++ src/Components/ListBooks.js
@@ -15,28 +15,28 @@ class ListBooks extends Component {
         wantToRead: ['Want to Read', 'wantToRead'],
         read: ['Read', 'read'],
       }
-        return (
-         <div className="list-books">
-           <div className="list-books-title">
-             <h1>MyReads</h1>
-           </div>
-           {Object.keys(shelves).map((shelf) => 
-           <BookShelf 
-             key={shelf}
-             books={this.props.books} 
-             onChangeShelf={this.props.onChangeShelf} 
-             title={shelves[shelf][0]} 
-             shelf={shelves[shelf][1]}
-             />
-                 )}
-               </div>
-               <div className="open-search">
-                 <Link to="/search">Add a book</Link>
-               </div>
-             </div>
-           )
-         }
-       }
-
+      return (
+    <div className="list-books">
+      <div className="list-books-title">
+        <h1>MyReads</h1>
+      </div>
+      <div className="list-books-content">
+        {Object.keys(shelves).map((shelf) => 
+          <BookShelf 
+            key={shelf}
+            books={this.props.books} 
+            onChangeShelf={this.props.onChangeShelf} 
+            title={shelves[shelf][0]} 
+            shelf={shelves[shelf][1]}
+          />
+        )}
+      </div>
+      <div className="open-search">
+        <Link to="/search">Add a book</Link>
+      </div>
+    </div>
+  )
+}
+}
 
 export default ListBooks
