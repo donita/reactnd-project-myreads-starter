d30f5ffae3d203865bb257619806048dab133c00
Donita <mathisdonita@gmail.com>
2018-10-08 04:11:02 -0400

    fix: fix bugs in Book js

--- src/Components/Book.js
+++ src/Components/Book.js
@@ -6,18 +6,10 @@ class Book extends Component {
     book: PropTypes.object.isRequired,
     onChangeShelf: PropTypes.func.isRequired
   }
-  
-  onShelfChange = (e) => {
-		e.preventDefault();
-		const {book, onChangeShelf} = this.props;
-		const shelf = e.target.value;
-		onChangeShelf(book, shelf);
-	}
-
 
 
   render() {
-    const {book} = this.props;
+    const {onChangeShelf, book} = this.props;
     return (
       <li>
         <div className="book">
@@ -25,7 +17,12 @@ class Book extends Component {
           <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks && book.imageLinks.thumbnail})`
 					}}></div>
           <div className="book-shelf-changer">
-              <select onChange={this.onShelfChange} defaultValue={book.shelf}>
+          <select
+         onChange={(event) => (
+           onChangeShelf(book,event.target.value)
+         )}
+         defaultValue={book.shelf ? book.shelf : 'none'}
+         >
             <option value="move" disabled>Move to...</option>
             <option value="currentlyReading">Currently Reading</option>
             <option value="wantToRead">Want to Read</option>
