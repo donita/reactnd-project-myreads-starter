28aaebcc71a87ff721377053de811839e47e4464
Donita <mathisdonita@gmail.com>
2018-10-08 04:23:57 -0400

    fix: bug fixes

--- src/App.js
+++ src/App.js
@@ -7,44 +7,43 @@ import SearchBooks from './Components/SearchBooks'
 
 class BooksApp extends React.Component {
   state = {
-    books: []
-  
+    books: [],
+    searchResults: []
   }
+
   componentDidMount() {
     BooksAPI.getAll().then((books) => {
-      this.setState({ books });
+      this.setState({ books })
     })
   }
 
   changeShelf = (book, shelf) => {
-   BooksAPI.update(book, shelf)
+    BooksAPI.update(book, shelf)
 
-   BooksAPI.getAll().then((books) => {
-     this.setState({ books })
-   })
- }
-    
+    BooksAPI.getAll().then((books) => {
+      this.setState({ books })
+    })
+  }
+
   render() {
-     const { books } = this.state
-
+    const { books, searchResults } = this.state
+    
     return (
       <div className="app">
         <Route exact path="/" render={() => (
-          <ListBooks 
-          books={books} 
-          onChangeShelf={book, shelf} => {
-            this.changeShelf(book, shelf)
-          }}
+          <ListBooks
+            books={books}
+            onChangeShelf={(book, shelf) => {
+              this.changeShelf(book, shelf)
+            }}
           />
         )}/>
-        
-        
-        <Route path="/search" render={( {history} ) => (
-          <SearchBooks 
-            books={books}
-            onChangeShelf={(book,shelf) => {
-              this.changeShelf(book,shelf) => {
-                this.changeShelf(book, shelf)
+        <Route path="/search" render={() => (
+          <SearchBooks
+            library={books}
+            results={searchResults}
+            onChangeShelf={(book, shelf) => {
+              this.changeShelf(book, shelf)
             }}
           />
         )}/>
@@ -52,4 +51,5 @@ class BooksApp extends React.Component {
     )
   }
 }
+
 export default BooksApp
