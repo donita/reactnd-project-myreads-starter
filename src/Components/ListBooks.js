66e4e9164ee97272aae2c34538e83013d3981516
Donita <mathisdonita@gmail.com>
2018-10-08 04:28:59 -0400

    fix: bug errors

--- src/Components/ListBooks.js
+++ src/Components/ListBooks.js
@@ -1,11 +1,11 @@
 import React, {Component} from 'react'
 import { Link } from 'react-router-dom'
+import PropTypes from 'prop-types'
 import Book from './Book'
 
 class ListBooks extends Component {
-
-  changeShelf = (event, book) => {
-    this.props.updateBookShelf(book, event.target.value)
+  static propTypes = {
+     books: PropTypes.array.isRequired,
   }
 
   render() {
@@ -17,44 +17,7 @@ class ListBooks extends Component {
           <h1>MyReads</h1>
         </div>
         <div className="list-books-content">
-          <div>
-            <div className="bookshelf">
-              <h2 className="bookshelf-title">Currently Reading</h2>
-              <div className="bookshelf-books">
-                <Book 
-                  shelf="currentlyReading"
-                  books={books} 
-                  toggleBookShelf={this.changeShelf}
-                />
-              </div>
-            </div>
-
-            <div className="bookshelf">
-              <h2 className="bookshelf-title">Want to Read</h2>
-              <div className="bookshelf-books">
-                <Book 
-                  shelf="wantToRead"
-                  books={books}
-                  toggleBookShelf={this.changeShelf}
-                />
-              </div>
-            </div>
-            <div className="bookshelf">
-              <h2 className="bookshelf-title">Read</h2>
-              <div className="bookshelf-books">
-                <Book 
-                  shelf="read"
-                  books={books}
-                  toggleBookShelf={this.changeShelf}
-                />
-              </div>
-            </div>
-          </div>
-        </div>
-        <div className="open-search">
-          <Link to="/search">Add a book</Link>
-        </div>
-      </div>
+          
     )
   }
 }
