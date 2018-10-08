6c4673ed5a7586499b17f1f43e4b72b9d2589b61
Donita <mathisdonita@gmail.com>
2018-10-08 05:43:30 -0400

    feat

--- src/Components/Book.js
+++ src/Components/Book.js
@@ -14,7 +14,7 @@ class Book extends Component {
       <li>
         <div className="book">
         <div className="book-top">
-          <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks.thumbnail})` }}></div>
+          <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${this.props.book.imageLinks.thumbnail})` }}></div>
           <div className="book-shelf-changer">
           <select
          onChange={(event) => (
